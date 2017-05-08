7a52ff1c0eb244776b3184db0c196d422795cd32
simplyrhythm <pussacha.nwn@gmail.com>
2017-05-09 02:29:51 +0700

    Initial edit form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,22 @@
 class UsersController < ApplicationController
   def me
+    sessionExist
+  end
+
+  def me_edit
+    sessionExist    
+  end
+
+  def update
+  end
+
+  private
+  def sessionExist
     if (session[:user])
       id = session[:user]['id']
       @user = User.find(id)
     else
       redirect_to('/login') and return
     end
-  end
-
-  def me_edit
   end
 end
